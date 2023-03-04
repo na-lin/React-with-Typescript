d943bbb59e20be4dc5ff98fbd1a4e8bf5267abdc
Na Lin <lna483018@gmail.com>
2023-03-04 12:01:54 -0500

    init react app with ts

---
+++ src/index.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Parent from "./props/Parent";
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(<Parent />);
