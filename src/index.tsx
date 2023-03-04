cd23feae2c9ba0efb8c23e0b381cf192b430ec6a
Na Lin <lna483018@gmail.com>
2023-03-04 12:10:23 -0500

    react input without ts

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import Parent from "./props/Parent";
+import GuestList from "./state/GuestList";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(<Parent />);
+root.render(<GuestList />);
