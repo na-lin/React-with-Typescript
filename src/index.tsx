ffe50f5df905bc15073baebc4cbbd11b6479b3f4
Na Lin <lna483018@gmail.com>
2023-03-06 23:18:09 -0500

    ref with ts

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,8 @@ import ReactDOM from "react-dom/client";
 import Parent from "./props/Parent";
 import GuestList from "./state/GuestList";
 import EventComponent from "./events/EventComponent";
-import UserSearch from "./state/UserSearch";
+import UserSearch from "./refs/UserSearch";
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
-root.render(<EventComponent />);
+root.render(<UserSearch />);
