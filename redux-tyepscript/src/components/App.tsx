3a5f4969f1cbf1cf89a4620ee8a50047c5f785d0
Na Lin <lna483018@gmail.com>
2023-03-08 00:05:06 -0500

    render component with Redux store

--- redux-tyepscript/src/components/App.tsx
+++ redux-tyepscript/src/components/App.tsx
@@ -1,7 +1,17 @@
 import React from "react";
+import { Provider } from "react-redux";
+import { store } from "../state";
+import RepositoriesList from "./RepositoriesList";
 
 function App() {
-  return <div>hi there</div>;
+  return (
+    <Provider store={store}>
+      <div>
+        <h1>Search for repositories</h1>
+        <RepositoriesList />
+      </div>
+    </Provider>
+  );
 }
 
 export default App;
