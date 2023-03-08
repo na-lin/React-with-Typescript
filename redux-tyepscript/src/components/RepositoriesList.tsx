0c547355db274572cb4bb8c9399a29508f6e85a5
Na Lin <lna483018@gmail.com>
2023-03-08 00:14:59 -0500

    dispatch action in component

--- redux-tyepscript/src/components/RepositoriesList.tsx
+++ redux-tyepscript/src/components/RepositoriesList.tsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { actionCreators } from "../state";
 
 const RepositoriesList: React.FC = () => {
+  const [term, setTerm] = useState("");
+  const dispatch = useDispatch();
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setTerm(event.target.value);
+  };
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    // call action creator
+    dispatch(actionCreators.searchRepositories(term) as any);
+  };
+
   return (
     <div>
-      <form>
-        <input />
+      <form onSubmit={handleSubmit}>
+        <input value={term} onChange={handleChange} />
         <button>Search</button>
       </form>
     </div>
