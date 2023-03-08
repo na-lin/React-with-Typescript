0c369dcb3408d8c2ba2fb68a9f4eb238a4900cf7
Na Lin <lna483018@gmail.com>
2023-03-08 00:35:28 -0500

    get state from redux store with state type

--- redux-tyepscript/src/components/RepositoriesList.tsx
+++ redux-tyepscript/src/components/RepositoriesList.tsx
@@ -1,9 +1,14 @@
 import React, { useState } from "react";
 import { useActions } from "../hook/useAction";
+import { useTypedSelector } from "../hook/useTypedSelector";
 
 const RepositoriesList: React.FC = () => {
   const [term, setTerm] = useState("");
   const { searchRepositories } = useActions();
+  const { loading, data, error } = useTypedSelector(
+    (state) => state.repositories
+  );
+  console.log(data);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setTerm(event.target.value);
